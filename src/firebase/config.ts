b0300ce65293d9c5c6715e01a39030347c813a98
Naman <namansoin000@gmail.com>
2025-11-11 04:16:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5623737926-eedcd",
+  "appId": "1:416706074817:web:ff89810c911c6ce251dc40",
+  "apiKey": "AIzaSyAoaKRcxzsszU7y5PeR6uU-LSusbeTQYyA",
+  "authDomain": "studio-5623737926-eedcd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "416706074817"
+};
